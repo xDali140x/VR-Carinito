a656ece2a575d16d0819edf16abf773e7e1e1922
xDali140x <est.samueld.gonzal1@unimilitar.edu.co>
2026-04-09 23:27:40 -0500

    sirve vela que se prende con briket

--- Assets/Scripts/vela.cs
+++ Assets/Scripts/vela.cs
@@ -1,16 +1,75 @@
 using UnityEngine;
 
-public class vela : MonoBehaviour
+public class Vela : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Triggers")]
+    public Collider sourceTrigger;      // Trigger del fuego origen
+    public Collider targetTrigger;      // Trigger de esta vela
+
+    [Header("Particulas")]
+    public ParticleSystem targetParticles;   // Fuego de la vela
+
+    [Header("Tiempo de contacto")]
+    public float requiredContactTime = 2f;
+
+    [Header("Configuracion")]
+    public bool lightOnlyOnce = true;
+    public bool isActivated = false;
+
+    private float contactTimer = 0f;
+
+    private void Start()
     {
-        
+        if (targetParticles != null)
+        {
+            targetParticles.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (lightOnlyOnce && isActivated)
+            return;
+
+        if (sourceTrigger == null || targetTrigger == null || targetParticles == null)
+            return;
+
+        bool triggersTouching = sourceTrigger.enabled &&
+                                targetTrigger.enabled &&
+                                sourceTrigger.bounds.Intersects(targetTrigger.bounds);
+
+        if (triggersTouching)
+        {
+            contactTimer += Time.deltaTime;
+
+            if (contactTimer >= requiredContactTime)
+            {
+                ActivateTargetFire();
+            }
+        }
+        else
+        {
+            contactTimer = 0f;
+        }
+    }
+
+    public void ActivateTargetFire()
+    {
+        if (targetParticles != null && !isActivated)
+        {
+            targetParticles.Play();
+            isActivated = true;
+        }
+    }
+
+    public void ResetActivation()
+    {
+        contactTimer = 0f;
+        isActivated = false;
+
+        if (targetParticles != null)
+        {
+            targetParticles.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
+        }
     }
 }
